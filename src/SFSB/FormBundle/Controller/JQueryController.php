0c771da20ba387b3372ade538eaf9d7dd405d77e
Sullivan SENECHAL <soullivaneuh@gmail.com>
2013-04-30 17:04:33 +0200

    Geolocation type

---
+++ src/SFSB/FormBundle/Controller/JQueryController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SFSB\FormBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/jquery")
+ */
+class JQueryController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+    
+    /**
+     * @Route("/geolocation")
+     * @Method({"GET", "POST"})
+     * @Template()
+     */
+    public function geolocationAction()
+    {
+        $form = $this->createFormBuilder()
+                ->add('geolocation', 'genemu_jquerygeolocation', array(
+                    'map'       => true,
+                    'latitude'  => array(
+                        'enabled'   => true,
+                        'hidden'    => false,
+                    ),
+                    'longitude' => array(
+                        'enabled'   => true,
+                        'hidden'    => false,
+                    ),
+                    'locality'  => array(
+                        'enabled'   => true,
+                        'hidden'    => false,
+                    ),
+                    'country'   => array(
+                        'enabled'   => true,
+                        'hidden'    => false,
+                    ),
+                ))
+                ->getForm()
+                ;
+        
+        return array(
+            'form'      => $form->createView(),
+        );
+    }
+}
+
+?>
